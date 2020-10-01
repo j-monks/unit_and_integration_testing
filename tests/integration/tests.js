cf511445ec104b7f0b30da580568f8754b588d18
James Monks <J.monks23@gmail.com>
2020-10-01 10:58:37 +0100

    large number calculation test pass

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -62,4 +62,17 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2.5')
   })
 
+  it('should display the correct output in regards to large numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('308025')
+  })
+
 });
