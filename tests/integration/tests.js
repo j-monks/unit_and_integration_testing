0dcfe169428483592fd671890310e08ad8738d72
James Monks <J.monks23@gmail.com>
2020-10-01 10:31:13 +0100

    update display running total test pass

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,4 +16,10 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should update display of running total', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
 });
