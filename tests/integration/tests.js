a91831a4fe661de98900c6dad95e51b122d9d8ee
James Monks <J.monks23@gmail.com>
2020-10-01 11:06:01 +0100

    won't divide by zero and displays message test pass

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -75,4 +75,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('308025')
   })
 
+  it('should display that you cant divide by zero', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal("Can't divide by 0");
+  })
+
 });
