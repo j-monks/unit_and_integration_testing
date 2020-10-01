69349889dd630130bcbc54cd180cca93fde8a1f5
James Monks <J.monks23@gmail.com>
2020-10-01 10:03:25 +0100

    subtract test pass

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -17,4 +17,10 @@ describe('calculator', function () {
     assert.equal(5, calculator.runningTotal);
   })
 
+  it('it can subtract', function(){
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.equal(3, calculator.runningTotal);
+  })
+
 });
